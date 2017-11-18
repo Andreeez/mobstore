b3c5d6e12f41afc1a1b92ec8d507e58abe6dcce5
Andreeez <32762098+Andreeez@users.noreply.github.com>
2017-11-18 17:10:44 +0100

    First set up and uploading to Git

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("hej");
+
+var listOfProducts;
+// add more global variables when needed..
+
+/* Get products from the json file and store it in a javascript variable */
+fetch("./products.json")
+.then(function(response) {
+    return response.json();
+})
+.then(function(products) {
+    listOfProducts = products;
+    createUIFromLoadedProducts();
+});
